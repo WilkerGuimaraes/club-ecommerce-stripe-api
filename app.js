6fdc3cae910f042d7581598ad1156df13ff5ee58
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-03-18 14:57:59 -0300

    feat: add vercel config

--- app.js
+++ app.js
@@ -34,4 +34,6 @@ app.post("/create-checkout-session", async (req, res) => {
   res.send({ url: session.url });
 });
 
-app.listen(5000, () => console.log("Running on port 5000"));
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log("Running on port 5000"));
